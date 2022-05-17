facedfc3d83090fc63857a8a00d54a485844496e
Perkles <fredericojunior1995@gmail.com>
2022-05-17 14:08:37 -0300

    adicionado novo botão de copiar

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,6 @@ botaoCopia.onclick = (copia);
 
 function copia() {
     var copia = (document.querySelector(".cartao__botao--copiar"));
-    copia.value = "https://pmv-ads-2022-1-e1-proj-web-t6-grupo-6-inicianteemprogramacao.vercel.app/";
+    copia.value = "https://fredsilva7l.github.io/PUC-Minas/";
     navigator.clipboard.writeText(copia.value);
 }
